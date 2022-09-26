69222af7fb5370b8b72ebb51c348a0088937a78e
Andrea McDougald <andrea0nly@gmail.com>
2022-09-26 10:51:44 -0600

    GTM

--- src/components/ShowPrice.js
+++ src/components/ShowPrice.js
@@ -21,7 +21,10 @@ export default function ShowPrice() {
               <span className=" text-sage">+ See Pricing</span>
             </h2>
           </div>
-          <div className="animate-[movein_.2s_ease-in_forwards] ">
+          <div
+            className="animate-[movein_.2s_ease-in_forwards] "
+            id="price_visible"
+          >
             <h2 className="min-w-max text-center absolute text-3xl font-bold tracking-tight text-gray-900 sm:block sm:text-4xl lg:text-left  ">
               Subscription only{" "}
               <span className=" text-sage">{price} month</span>
